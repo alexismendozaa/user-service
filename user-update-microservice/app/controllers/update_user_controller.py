51ccf3fceb9252d1c3114ebef790ae0f0ee3b473
Alexis <afmendozaf@uce.edu.ec>
2025-02-11 12:39:48 -0500

    otro cambio en controlador jsjs

--- user-update-microservice/app/controllers/update_user_controller.py
+++ user-update-microservice/app/controllers/update_user_controller.py
@@ -3,22 +3,21 @@ from app.models.user import User
 
 def update_user(db: Session, user_id: int, username: str = None, email: str = None):
     user = db.query(User).filter(User.id == user_id).first()
+    
     if not user:
         return {"error": "Usuario no encontrado"}
 
-    updated = False  # Flag para saber si hubo cambios
-
+    update_data = {}
+    
     if username and user.username != username:
-        user.username = username
-        updated = True
+        update_data["username"] = username
 
     if email and user.email != email:
-        user.email = email
-        updated = True
+        update_data["email"] = email
 
-    if updated:
-        db.commit()  # Guardar solo si hubo cambios
-        db.refresh(user)  # Asegurar que los datos reflejan la BD
-
+    if update_data:
+        db.query(User).filter(User.id == user_id).update(update_data)
+        db.commit()
+        user = db.query(User).filter(User.id == user_id).first()  # Obtener el usuario actualizado
+    
     return {"message": "Usuario actualizado con éxito", "user": user}
-
